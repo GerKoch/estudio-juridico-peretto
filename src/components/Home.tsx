c8624d82050748f9d040447a9608ae2a721cb6bb
Your Name <gjkoch7@gmail.com>
2025-02-25 16:12:49 -0300

    Create robots.txt

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -18,7 +18,7 @@ const Home: React.FC = () => {
           <div className="pt-12">
             <button
               onClick={() => window.open("https://wa.me/5493364339538?text=Hola quiero hacerte una consulta")}
-              className="text-center text-white font-[roboto_slab] text-xl border-2 border-slate-200 px-10 py-2 rounded-md hover:bg-slate-200 hover:text-slate-900"
+              className="text-center text-white font-[roboto_slab] text-xl border-2 border-slate-200 px-10 py-2 rounded-md hover:bg-slate-200 hover:text-slate-900 transition-all duration-300 ease-in-out"
             >
               Contacto
             </button>
